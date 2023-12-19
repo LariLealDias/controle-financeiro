a42ef84cdd29b76d23a0ed791265223c1a8f24cd
hgbeleza <developer.beleza06@gmail.com>
2023-12-19 01:52:28 -0300

    refactor(hgbeleza): refatoração do código

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect('mongodb+srv://Controle-Financeiro:123@cf.eynyyo5.mongodb.net/Controle-Financeiro?');
-
-let db = mongoose.connection;
-
-export default db;
